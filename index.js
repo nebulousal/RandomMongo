8444336f2d41aa1e9e185369aefd8328f1ede0c3
Alain Nisam <alain@yourmlssearch.com>
2016-10-30 09:51:03 -0500

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
-
 exports.populateRandomMongo = function(count,options) {
 	var MongoClient = require('mongodb').MongoClient;
 	var loremIpsum = require('lorem-ipsum');
